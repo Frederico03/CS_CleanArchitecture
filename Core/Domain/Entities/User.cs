c8af4dbb7f07b88659b6e2e2f3f975b3f3e78285
Frederico03 <fredericogarcez13@gmail.com>
2024-05-20 19:59:59 -0300

    add first video

---
+++ Core/Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+namespace Domain.Entities
+{
+    public sealed class User
+    {
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ Core/Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,7 @@
+namespace Domain.Interfaces
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<User> GetByEmail(string email, CancellationToken cancellationToken);
+    }
+}
